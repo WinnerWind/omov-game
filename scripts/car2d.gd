b604d99cdae4c0756ccdaa34a192fd2304888c98
WinnerWind <contact@winnerwind.in>
2025-10-17 16:18:02 +0530

    Basic car movement

---
+++ scripts/car2d.gd
@@ -0,0 +1,26 @@
+@tool
+extends Path2D
+class_name CarPath
+
+@export var time_taken:float
+@export_group("Visuals")
+@export var texture:Texture2D:
+	set(new_texture):
+		texture = new_texture
+		if is_node_ready(): set_variables()
+@export var collision_shape:RectangleShape2D:
+	set(new_shape):
+		collision_shape = new_shape
+		if is_node_ready(): set_variables()
+@export_group("Nodes")
+@export var sprite:Sprite2D
+@export var collision_object:CollisionShape2D
+@export var path_follow:PathFollow2D
+
+func _process(delta: float) -> void:
+	path_follow.progress_ratio += delta / time_taken # Move object in time_taken seconds.
+func set_variables():
+	if sprite and texture:
+		sprite.texture = texture
+	if collision_object and collision_shape:
+		collision_object.shape = collision_shape
