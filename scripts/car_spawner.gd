b10da9ac953679d058c184045efc9c99361ca2f7
WinnerWind <contact@winnerwind.in>
2025-10-18 10:36:08 +0530

    Car spawner with randomized delay

--- scripts/car_spawner.gd
+++ scripts/car_spawner.gd
@@ -1,10 +1,22 @@
 extends Node2D
 class_name CarSpawner
 
-@export var car_packed:PackedScene
+@export var max_delay:float
+@export var min_delay:float
+var delay:
+	get:
+		return randf_range(min_delay,max_delay)
 @export var curves:Array[Curve2D]
+@export_group("Internal")
+@export var car_packed:PackedScene
+@export var timer:Timer
+
 func _ready() -> void:
-	for i in 10:
-		var car:CarPath = car_packed.instantiate()
-		car.curve = curves.pick_random()
-		add_child(car)
+	timer.timeout.connect(queue_spawn)
+	timer.start(delay)
+
+func queue_spawn() -> void:
+	var car:CarPath = car_packed.instantiate()
+	car.curve = curves.pick_random()
+	add_child(car)
+	timer.start(delay)
